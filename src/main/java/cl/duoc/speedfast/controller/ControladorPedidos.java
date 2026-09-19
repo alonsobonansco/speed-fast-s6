b1359a284131268df0a58b241378b14ee5b8674f
Alonso Bonansco <al.bonansco@duocuc.cl>
2026-09-19 17:51:32 -0300

    feat: completar ControladorPedidos creando interfaz LogListener

--- src/main/java/cl/duoc/speedfast/controller/ControladorPedidos.java
+++ src/main/java/cl/duoc/speedfast/controller/ControladorPedidos.java
@@ -1,4 +1,56 @@
 package cl.duoc.speedfast.controller;
 
+import cl.duoc.speedfast.model.Pedido;
+import cl.duoc.speedfast.service.LogListener;
+import cl.duoc.speedfast.service.Repartidor;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ControladorPedidos {
+
+    private final BlockingQueue<Pedido> pedidosPendientes = new LinkedBlockingQueue<>();
+    private LogListener logListener;
+
+    public void iniciarSimulacionReparto(List<Pedido> listaPedidos) {
+        if (listaPedidos == null || listaPedidos.isEmpty()) {
+            escribirMensaje("[AVISO] No hay pedidos registrados en el sistema para despachar.");
+            return;
+        }
+
+        escribirMensaje("\n --- INICIANDO REPARTO CONCURRENTE --- ");
+
+        pedidosPendientes.clear();
+        for (Pedido pedido : listaPedidos) {
+            agregarPedido(pedido);
+        }
+
+        String[] nombresRepartidores = {"Juan", "María", "Carlos"};
+        for (String nombre : nombresRepartidores) {
+            Thread hiloRepartidor = new Thread(new Repartidor(nombre, this));
+            hiloRepartidor.start();
+        }
+    }
+
+    private void agregarPedido(Pedido pedido) {
+        pedidosPendientes.add(Objects.requireNonNull(
+                pedido, "El pedido no puede ser nulo."
+        ));
+    }
+
+    public Pedido retirarPedido() {
+        return pedidosPendientes.poll();
+    }
+
+    public void escribirMensaje(String mensaje) {
+        if (logListener != null) {
+            logListener.onLog(mensaje);
+        }
+    }
+
+    public boolean estaVacia() {
+        return pedidosPendientes.isEmpty();
+    }
 }
